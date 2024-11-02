e53abde03c4aff2f5b8c97a533722317e3e4853c
Lina Gómez <77931393+Lina-go@users.noreply.github.com>
2024-11-02 12:52:16 -0500

    commit

--- SustainU/Views/ContentView.swift
+++ SustainU/Views/ContentView.swift
@@ -4,20 +4,6 @@ struct ContentView: View {
     @StateObject private var loginViewModel = LoginViewModel.shared
 
     var body: some View {
-        NavigationView {
-                HomeView(userProfile: loginViewModel.userProfile, isAuthenticated: $loginViewModel.isAuthenticated)
-            }
-        }
-    }
-
-struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
-    }
-        if loginViewModel.isAuthenticated {
             HomeView(userProfile: loginViewModel.userProfile)
-        } else {
-            LoginView(viewModel: loginViewModel)
-        }
     }
 }
